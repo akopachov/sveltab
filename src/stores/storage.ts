0e43b86a4560a3a1006346264843fd15006ec90b
alex.kopachov <alex.kopachov@gmail.com>
2024-01-04 22:39:28 +0100

    feat: things for Netlify

--- src/stores/storage.ts
+++ src/stores/storage.ts
@@ -2,7 +2,7 @@ import type { Storage } from 'webextension-polyfill';
 
 let $$storage: Storage.Static | null = null;
 
-if (import.meta.env.DEV || import.meta.env.SSR) {
+if (import.meta.env.DEV || import.meta.env.SSR || import.meta.env.BUILD_FOR_WEBSITE) {
   $$storage = <Storage.Static>(await import('browser-storage-polyfill/index')).default;
 } else {
   $$storage = (await import('webextension-polyfill')).default.storage;
