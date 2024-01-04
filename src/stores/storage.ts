6dd93ab2c7e75a69093facfb443f607f591e846f
alex.kopachov <alex.kopachov@gmail.com>
2024-01-04 22:45:18 +0100

    few more thing for netlify

--- src/stores/storage.ts
+++ src/stores/storage.ts
@@ -2,10 +2,22 @@ import type { Storage } from 'webextension-polyfill';
 
 let $$storage: Storage.Static | null = null;
 
-if (import.meta.env.DEV || import.meta.env.SSR || import.meta.env.BUILD_FOR_WEBSITE) {
+async function usePolyfill() {
   $$storage = <Storage.Static>(await import('browser-storage-polyfill/index')).default;
-} else {
+}
+
+async function useWebExt() {
   $$storage = (await import('webextension-polyfill')).default.storage;
+}
+
+if (import.meta.env.DEV || import.meta.env.SSR) {
+  await usePolyfill();
+} else {
+  try {
+    await useWebExt();
+  } catch {
+    await usePolyfill();
+  }
 }
 
 export const storage = $$storage;
