b6fbcfb37f0564c024800a0cbb7708ba59cf0d5f
alex.kopachov <alex.kopachov@gmail.com>
2024-01-29 15:08:54 +0100

    fix(i18n): few improvements to `be` locale

--- webextension/src/shared-components/language-selector.svelte
+++ webextension/src/shared-components/language-selector.svelte
@@ -3,10 +3,15 @@
   import { locale } from '$stores/locale';
 
   $: langDisplayNames = new Intl.DisplayNames([$locale], { type: 'language' });
+
+  function pascalCase(s: string | undefined) {
+    if (!s) return s;
+    return `${s[0].toUpperCase()}${s.slice(1)}`;
+  }
 </script>
 
 <select class="select" bind:value={$locale}>
   {#each availableLanguageTags as lang}
-    <option value={lang}>{langDisplayNames.of(lang)}</option>
+    <option value={lang}>{pascalCase(langDisplayNames.of(lang))}</option>
   {/each}
 </select>
