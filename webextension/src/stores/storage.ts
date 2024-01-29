5c14e2dfb1c331d32cc5ad395dca60caac7eca3a
alex.kopachov <alex.kopachov@gmail.com>
2024-01-29 09:38:19 +0100

    chore(Core): updated packages

--- webextension/src/stores/storage.ts
+++ webextension/src/stores/storage.ts
@@ -3,7 +3,20 @@ import type { Storage } from 'webextension-polyfill';
 let $$storage: Storage.Static | null = null;
 
 async function usePolyfill() {
-  $$storage = <Storage.Static>(await import('browser-storage-polyfill/index')).default;
+  const { setStorage, getStorage, removeStorage, clearStorage } = await import(
+    'browser-storage-polyfill/src/helpers/index'
+  );
+  const polyFillStorage = {
+    set: setStorage,
+    get: getStorage,
+    remove: removeStorage,
+    clear: clearStorage,
+  };
+  $$storage = <any>{
+    local: polyFillStorage,
+    session: polyFillStorage,
+    sync: polyFillStorage,
+  };
 }
 
 async function useWebExt() {
