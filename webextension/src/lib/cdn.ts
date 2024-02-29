e225ed7a942ee06d306d83443a6531b382d7b784
alex.kopachov <alex.kopachov@gmail.com>
2024-02-29 08:20:59 +0100

    fix: sometimes images from wikimedia are not served by imagecdn.app

--- webextension/src/lib/cdn.ts
+++ webextension/src/lib/cdn.ts
@@ -1,4 +1,8 @@
-const ImageCdnAppBlacklistedChars = /[ŽžÀ-ÿ()]/;
+// What imagecdn.app doesn't like
+const ImageCdnAppBlacklist = [
+  /[ŽžÀ-ÿ()]/, // characters with an accent or diacritic mark as well as few other chars
+  /^https:\/\/upload.wikimedia.org/, // sometimes it may not like images from upload.wikimedia.org
+] as const;
 
 export function getImageCdnUrl(
   imgUrl: string | undefined | null,
@@ -10,10 +14,11 @@ export function getImageCdnUrl(
   }
 
   const encodedUrl = encodeURIComponent(imgUrl);
+  const pureDecodedUrl = decodeURIComponent(imgUrl);
 
   let cdnUrl: string;
-  if (ImageCdnAppBlacklistedChars.test(decodeURIComponent(imgUrl))) {
-    // imagecdn.app doesn't like characters with an accent or diacritic mark as well as few other chars, so let's use another CDN that case
+  if (ImageCdnAppBlacklist.some(b => b.test(pureDecodedUrl))) {
+    // if unlikely imagecdn.app would server given url - let's use another CDN
     cdnUrl = `https://demo.tiny.pictures/?source=${encodedUrl}&resizeType=cover&format=webp&progressive=true&optimize=true`;
   } else {
     cdnUrl = `https://imagecdn.app/v2/image/${encodedUrl}?format=webp`;
