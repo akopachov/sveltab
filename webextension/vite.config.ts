1eff1ab02a5ea4b7cc86c1c3cc6f0f50ff67ec01
alex.kopachov <alex.kopachov@gmail.com>
2024-03-12 17:39:11 +0100

    chore: fixed build after refactoring

--- webextension/vite.config.ts
+++ webextension/vite.config.ts
@@ -6,27 +6,37 @@ import { run } from 'vite-plugin-run';
 import glob from 'tiny-glob';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath, pathToFileURL } from 'node:url';
+import fs from 'node:fs';
 
-const generators = [];
-const generatorFiles = await glob('src/**/*.gen.mjs', { cwd: __dirname, absolute: true, dot: true, filesOnly: true });
-for (const generatorFile of generatorFiles) {
-  console.log(`Loading generator from ${generatorFile}`);
-  const generatorFileUrl = pathToFileURL(generatorFile).toString();
-  const generator = await import(generatorFileUrl).then(m => m.default);
-  const generatorDir = dirname(fileURLToPath(generatorFileUrl));
-  const watchPatterns = (generator.pattern || []).map((p: string) => resolve(generatorDir, p));
-  generators.push(
-    run({
-      input: [
-        {
-          name: generator.name,
-          run: [process.argv[0], '--env-file', '.env', '-e', `import("${generatorFileUrl}").then(a => a.main());`],
-          pattern: [generatorFile, ...watchPatterns],
-        },
-      ],
-      silent: false,
-    }),
-  );
+async function getGenerators() {
+  const generators = [];
+  const generatorFiles = await glob('src/**/*.gen.mjs', { cwd: __dirname, absolute: true, dot: true, filesOnly: true });
+  const envFileArgs = [];
+  if (fs.existsSync('.env')) {
+    envFileArgs.push('--env-file', '.env');
+  }
+
+  for (const generatorFile of generatorFiles) {
+    console.log(`Loading generator from ${generatorFile}`);
+    const generatorFileUrl = pathToFileURL(generatorFile).toString();
+    const generator = await import(generatorFileUrl).then(m => m.default);
+    const generatorDir = dirname(fileURLToPath(generatorFileUrl));
+    const watchPatterns = (generator.pattern || []).map((p: string) => resolve(generatorDir, p));
+    generators.push(
+      run({
+        input: [
+          {
+            name: generator.name,
+            run: [process.argv[0], ...envFileArgs, '-e', `import("${generatorFileUrl}").then(a => a.main());`],
+            pattern: [generatorFile, ...watchPatterns],
+          },
+        ],
+        silent: false,
+      }),
+    );
+  }
+
+  return generators;
 }
 
 export default defineConfig({
@@ -37,7 +47,7 @@ export default defineConfig({
   },
   plugins: [
     sveltekit(),
-    ...generators,
+    ...(await getGenerators()),
     paraglide({
       project: './project.inlang',
       outdir: './src/i18n/generated',
