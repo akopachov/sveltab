8394c6a0d0160c9d40d2c68e78d5106fcae6a242
alex.kopachov <alex.kopachov@gmail.com>
2024-03-26 12:49:33 +0100

    chore: owm-redirect worker\function small refactoring

--- apps/owm-redirect/src/worker.ts
+++ apps/owm-redirect/src/worker.ts
@@ -2,35 +2,46 @@ export interface Env {
   OWM_API_KEY: string;
 }
 
+type OWMFindResponseResultItem = {
+  id: number;
+  coord: { lat: number; lon: number };
+};
+type OWMFindResponse = { list: OWMFindResponseResultItem[] };
+
+function getFailedResponse() {
+  return Response.redirect("https://openweathermap.org/", 308);
+}
+
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
     const url = new URL(request.url);
-    const city = url.searchParams.get('city');
-    const country = url.searchParams.get('country');
-    const lat = Number(url.searchParams.get('lat'));
-    const lon = Number(url.searchParams.get('lon'));
-    if (city && country) {
-      const q = encodeURIComponent(`${city}, ${country}`);
-      const owmResponse = await fetch(`https://openweathermap.org/data/2.5/find?q=${q}&appid=${env.OWM_API_KEY}`).then(
-        r => r.json(),
-      );
-      if (!owmResponse || !owmResponse.list || owmResponse.list.length <= 0) {
-        return Response.redirect('https://openweathermap.org/', 308);
-      }
-
-      let cityId;
-      if (owmResponse.list.length === 1) {
-        cityId = owmResponse.list[0].id;
-      } else {
-        const idList = owmResponse.list.map(item => ({
-          id: item.id,
-          diff: Math.abs(item.coord.lat - lat) + Math.abs(item.coord.lon - lon),
-        }));
-        idList.sort((a, b) => a.diff - b.diff);
-        cityId = idList[0].id;
-      }
-      return Response.redirect(`https://openweathermap.org/city/${cityId}`, 308);
+    const city = url.searchParams.get("city");
+    const country = url.searchParams.get("country");
+    const lat = Number(url.searchParams.get("lat"));
+    const lon = Number(url.searchParams.get("lon"));
+    if (!city || !country) {
+      return getFailedResponse();
     }
-    return Response.redirect('https://openweathermap.org/', 308);
+
+    const q = encodeURIComponent(`${city}, ${country}`);
+    const owmResponse = await fetch(
+      `https://openweathermap.org/data/2.5/find?q=${q}&appid=${env.OWM_API_KEY}`,
+    ).then<OWMFindResponse>((r) => r.json());
+    if (!owmResponse || !owmResponse.list || owmResponse.list.length <= 0) {
+      return getFailedResponse();
+    }
+
+    let cityId;
+    if (owmResponse.list.length === 1) {
+      cityId = owmResponse.list[0].id;
+    } else {
+      const idList = owmResponse.list.map((item) => ({
+        id: item.id,
+        diff: Math.abs(item.coord.lat - lat) + Math.abs(item.coord.lon - lon),
+      }));
+      idList.sort((a, b) => a.diff - b.diff);
+      cityId = idList[0].id;
+    }
+    return Response.redirect(`https://openweathermap.org/city/${cityId}`, 308);
   },
 };
--- apps/preview/functions/owm-redirect.mts
+++ apps/preview/functions/owm-redirect.mts
@@ -12,34 +12,39 @@ type OWMFindResponseResultItem = {
 };
 type OWMFindResponse = { list: OWMFindResponseResultItem[] };
 
+function getFailedResponse() {
+  return Response.redirect("https://openweathermap.org/", 308);
+}
+
 export default async (request: Request) => {
   const url = new URL(request.url);
   const city = url.searchParams.get("city");
   const country = url.searchParams.get("country");
   const lat = Number(url.searchParams.get("lat"));
   const lon = Number(url.searchParams.get("lon"));
-  if (city && country) {
-    const q = encodeURIComponent(`${city}, ${country}`);
-    const apiKey = Netlify.env.get("OWM_API_KEY");
-    const owmResponse = await fetch(
-      `https://openweathermap.org/data/2.5/find?q=${q}&appid=${apiKey}`,
-    ).then<OWMFindResponse>((r) => r.json());
-    if (!owmResponse || !owmResponse.list || owmResponse.list.length <= 0) {
-      return Response.redirect("https://openweathermap.org/", 308);
-    }
-
-    let cityId;
-    if (owmResponse.list.length === 1) {
-      cityId = owmResponse.list[0].id;
-    } else {
-      const idList = owmResponse.list.map((item) => ({
-        id: item.id,
-        diff: Math.abs(item.coord.lat - lat) + Math.abs(item.coord.lon - lon),
-      }));
-      idList.sort((a, b) => a.diff - b.diff);
-      cityId = idList[0].id;
-    }
-    return Response.redirect(`https://openweathermap.org/city/${cityId}`, 308);
+  if (!city || !country) {
+    return getFailedResponse();
   }
-  return Response.redirect("https://openweathermap.org/", 308);
+
+  const q = encodeURIComponent(`${city}, ${country}`);
+  const apiKey = Netlify.env.get("OWM_API_KEY");
+  const owmResponse = await fetch(
+    `https://openweathermap.org/data/2.5/find?q=${q}&appid=${apiKey}`,
+  ).then<OWMFindResponse>((r) => r.json());
+  if (!owmResponse || !owmResponse.list || owmResponse.list.length <= 0) {
+    return getFailedResponse();
+  }
+
+  let cityId;
+  if (owmResponse.list.length === 1) {
+    cityId = owmResponse.list[0].id;
+  } else {
+    const idList = owmResponse.list.map((item) => ({
+      id: item.id,
+      diff: Math.abs(item.coord.lat - lat) + Math.abs(item.coord.lon - lon),
+    }));
+    idList.sort((a, b) => a.diff - b.diff);
+    cityId = idList[0].id;
+  }
+  return Response.redirect(`https://openweathermap.org/city/${cityId}`, 308);
 };
