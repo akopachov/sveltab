a34dd7c2bd041ffe7b9eac2cb6107f2cd6cf225f
alex.kopachov <alex.kopachov@gmail.com>
2024-03-27 10:29:17 +0100

    ci: updating github pipeline

--- apps/preview/plugins/netlify-plugin-interpolate-env-variable/index.js
+++ apps/preview/plugins/netlify-plugin-interpolate-env-variable/index.js
@@ -6,16 +6,22 @@ export async function onPreBuild({ inputs, utils }) {
   if (inputs.variable && process.env[inputs.variable]) {
     log(`Interpolating environment variable ${inputs.variable}`);
     const envVariable = process.env[inputs.variable];
-    const { stdout: interpolated } = await utils.run("echo", [envVariable], {
-      env: process.env,
-      shell: true,
-      stdout: "pipe",
-    });
-    process.env[inputs.variable] = interpolated;
-    log(`Environmental variable ${inputs.variable} has been updated`);
+    try {
+      const { stdout: interpolated } = await utils.run("echo", [envVariable], {
+        env: process.env,
+        shell: true,
+        stdout: "pipe",
+      });
+      process.env[inputs.variable] = interpolated;
+      log(`Environmental variable ${inputs.variable} has been updated`);
+    } catch (error) {
+      build.failBuild("An error occurred during env variable interpolation", {
+        error,
+      });
+    }
   } else {
     utils.build.failBuild(
-      "Input variable is not defined or does not exist in the environment variables.",
+      "Input variable is not defined or does not exist in the environment variables",
     );
   }
 }
