a5b063b939020e15920b5cead33002f335c0ba53
alex.kopachov <alex.kopachov@gmail.com>
2024-03-27 12:50:55 +0100

    feat(Core): better color picker

--- apps/webextension/src/shared-components/color-picker.svelte
+++ apps/webextension/src/shared-components/color-picker.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
   import { popup, type PopupSettings } from '@skeletonlabs/skeleton';
   import { nanoid } from 'nanoid/non-secure';
-  import ColorPicker from 'svelte-awesome-color-picker';
+  import { onDestroy } from 'svelte';
+  import Picker from 'vanilla-picker/csp';
+  import 'vanilla-picker/dist/vanilla-picker.csp.css';
 
   export let color: string;
   export let inline: boolean = false;
 
+  let pickerParent: HTMLElement;
   let popupVisible: boolean = false;
+  let picker: Picker;
+
+  $: {
+    initializePicker(pickerParent);
+  }
 
   let popupSettings: PopupSettings = {
     event: 'click',
@@ -18,14 +26,34 @@
         fallbackAxisSideDirection: 'start',
       },
     },
-    state: v => (popupVisible = v.state),
+    state: v => {
+      popupVisible = v.state;
+    },
   };
+
+  onDestroy(() => {
+    picker?.destroy();
+    picker = null;
+  });
+
+  function initializePicker(parent: HTMLElement) {
+    picker?.destroy();
+    picker = null;
+    if (parent) {
+      picker = new Picker({
+        popup: false,
+        parent: parent,
+        color: color,
+        onChange: (c: any) => {
+          color = c.hex;
+        },
+      });
+    }
+  }
 </script>
 
 {#if inline}
-  <div>
-    <ColorPicker bind:hex={color} label="" isDialog={false} />
-  </div>
+  <div bind:this={pickerParent}></div>
 {:else}
   <button
     class="btn rounded-full !p-0 w-6 h-6 bg-contain bg-[url('/transparent-sm.png')] {$$restProps.class || ''}"
@@ -34,19 +62,15 @@
     style:box-shadow="inset 0 0 0 1.5rem {color}">
   </button>
   <div
-    class="card shadow-xl w-fit h-fit overflow-y-auto flex z-[99999]"
+    class="card shadow-xl w-fit h-fit overflow-y-auto flex z-[99999] px-1"
     tabindex="-1"
     style:visibility={popupVisible ? 'visible' : 'hidden'}
     data-popup={popupSettings.target}>
     {#if popupVisible}
-      <ColorPicker bind:hex={color} label="" isDialog={false} />
+      <div bind:this={pickerParent} class="contents"></div>
     {/if}
   </div>
 {/if}
 
 <style lang="postcss">
-  :global(.color-picker) {
-    --picker-width: min(max(10cqmin, 150px), 250px);
-    --picker-height: min(max(10cqmin, 150px), 250px);
-  }
 </style>
