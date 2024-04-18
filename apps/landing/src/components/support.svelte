325abee713211eb13a85e1189fa1636463758dd2
alex.kopachov <alex.kopachov@gmail.com>
2024-04-18 15:29:45 +0200

    feat(Landing): added "support" section

---
+++ apps/landing/src/components/support.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+</script>
+
+<section class="relative" id="support">
+  <div class="relative max-w-6xl mx-auto px-4 sm:px-6">
+    <div class="py-12 md:py-20">
+      <!-- Section header -->
+      <div class="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+        <h2 class="h2 mb-4">Support</h2>
+      </div>
+      <p class="text-xl text-gray-600">Want to say thank you and donate some funds?</p>
+      <p class="text-xl text-gray-600">We highly appreciate your decision. Here are some ways how you can do it:</p>
+      <ul class="list-disc mt-5 ml-8 leading-loose">
+        <li>
+          Buy me a coffee <a class="link" href="https://ko-fi.com/akopachov">here</a>
+          (No account needed, one-time)
+        </li>
+        <li>
+          Become a patron at <a class="link" href="https://patreon.com/akopachov">Patreon</a>
+          (Account needed)
+        </li>
+        <li>
+          Z.Cash: <pre class="kbd kbd-md inline">t1PCzJrd96RUfzjzhBERfXEFvSi7W6V86hM</pre>
+        </li>
+        <li>
+          DOGE: <pre class="kbd kbd-md inline">DAa3nu1RCWwxZdAnGVga77bgxDFP1nhahj</pre>
+        </li>
+        <li>
+          USDT: <pre class="kbd kbd-md inline">0xa12163eD56e35d3B38F7087B573384E40b2785e1</pre>
+        </li>
+        <li>
+          TON: <pre class="kbd kbd-md inline">UQCSBzoTb1B7RhXnka5RegmdjHR3gQwRVgZHNPPqzjjvlTKW</pre>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
--- apps/landing/src/routes/+page.svelte
+++ apps/landing/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   import Welcome from '../components/welcome.svelte';
   import Features from '../components/features.svelte';
+  import Support from '$components/support.svelte';
 </script>
 
 <Welcome />
 <Features />
+<Support />
