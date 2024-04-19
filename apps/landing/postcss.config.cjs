39a246a14eb3de07e1e0879d9951c724a31a0a93
alex.kopachov <alex.kopachov@gmail.com>
2024-04-19 10:14:02 +0200

    chore(Landing): added CSS Purge and minification

--- apps/landing/postcss.config.cjs
+++ apps/landing/postcss.config.cjs
@@ -1,13 +1,18 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 const config = {
-	plugins: [
-		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-		tailwindcss(),
-		//But others, like autoprefixer, need to run after,
-		autoprefixer
-	]
+  plugins: [
+    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+    tailwindcss(),
+    //But others, like autoprefixer, need to run after,
+    autoprefixer,
+    cssnano({
+      preset:
+        process.env.NODE_ENV === 'production' ? require('cssnano-preset-default') : require('cssnano-preset-lite'),
+    }),
+  ],
 };
 
 module.exports = config;
--- apps/landing/vite.config.ts
+++ apps/landing/vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 export default defineConfig({
-  plugins: [enhancedImages(), sveltekit()],
+  plugins: [enhancedImages(), sveltekit(), purgeCss()],
 });
