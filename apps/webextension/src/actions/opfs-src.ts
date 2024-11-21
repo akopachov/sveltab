1a7b0e67ca0e04f180723c54ae1006ca492dce15
alex.kopachov <alex.kopachov@gmail.com>
2024-11-21 11:08:30 +0100

    feat(Webextension): Increased OPFS loader BLOB strategy threshold to 100KiB
    as ChatGPT suggested

--- apps/webextension/src/actions/opfs-src.ts
+++ apps/webextension/src/actions/opfs-src.ts
@@ -5,7 +5,7 @@ import type { Action } from 'svelte/action';
 type HTMLElementWithRef = HTMLElement & ({ src: string | undefined } | { href: string | undefined });
 
 const log = logger.getSubLogger({ prefix: ['OPFS source loader:'] });
-const BLOB_STRATEGY_THRESHOLD = 16_384; // 16KiB
+const BLOB_STRATEGY_THRESHOLD = 102_400; // 100KiB
 
 export const opfsSrc: Action<HTMLElementWithRef, string | undefined> = function (
   node: HTMLElementWithRef,
