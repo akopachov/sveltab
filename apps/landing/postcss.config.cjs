2743f4b68fec02c45d1fe0a71ce3098696d77cc8
alex.kopachov <alex.kopachov@gmail.com>
2025-03-04 14:16:37 +0100

    chore(landing): upgraded to Tailwind v4 and Daisy v5

--- apps/landing/postcss.config.cjs
+++ apps/landing/postcss.config.cjs
@@ -1,13 +1,9 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const tailwind = require('@tailwindcss/postcss');
 
 const config = {
   plugins: [
-    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-    tailwindcss(),
-    //But others, like autoprefixer, need to run after,
-    autoprefixer,
+    tailwind(),
     cssnano({
       preset:
         process.env.NODE_ENV === 'production' ? require('cssnano-preset-default') : require('cssnano-preset-lite'),
