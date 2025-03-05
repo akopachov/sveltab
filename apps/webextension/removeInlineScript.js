4128dba6e1468dd1d817ef1597548ac55d46c029
alex.kopachov <alex.kopachov@gmail.com>
2025-03-05 13:05:37 +0100

    chore(Webextension): added HTML minification for index.html

--- apps/webextension/removeInlineScript.js
+++ apps/webextension/removeInlineScript.js
@@ -3,6 +3,7 @@ import path from 'node:path';
 import fs from 'node:fs/promises';
 import revisionHash from 'rev-hash';
 import { minify } from '@putout/minify';
+import minifyHtml from '@minify-html/node';
 
 async function removeInlineScript(directory) {
   console.log('Removing Inline Scripts');
@@ -17,7 +18,7 @@ async function removeInlineScript(directory) {
     let fileContent = await fs.readFile(file, { encoding: 'utf-8' });
 
     const scriptRegx = /<script[^>]*>([\s\S]+?)<\/script>/gi;
-    const newHtml = fileContent.replace(scriptRegx, (m, script) => {
+    let newHtml = fileContent.replace(scriptRegx, (m, script) => {
       if (script) {
         const inlineContent = minify(
           script
@@ -32,6 +33,18 @@ async function removeInlineScript(directory) {
         return `<script type="module" src="/${scriptFileName}"></script>`;
       }
     });
+
+    const minifiedNewHtmlBuffer = minifyHtml.minify(Buffer.from(newHtml), {
+      keep_closing_tags: true,
+      keep_html_and_head_opening_tags: true,
+      keep_spaces_between_attributes: true,
+      keep_comments: false,
+      do_not_minify_doctype: true,
+      ensure_spec_compliant_unquoted_attribute_values: true,
+      minify_js: false,
+      minify_css: false,
+    });
+    newHtml = minifiedNewHtmlBuffer.toString('utf-8');
 
     await fs.writeFile(file, newHtml);
     console.groupEnd();
